e7b6dfe501706d110b95ef783709e16f598ae6b5
Егор Пилипенко <egor.pilipenko.1998@gmail.com>
2018-02-21 16:52:48 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-module.exports = function solveEquation(equation) {
-  // your implementation
+module.exports = function solveEquation(equation){
+    let x1 = 0;
+    let x2 = 0;
+    let result =[];
+    let Urav = equation.split(' ');
+    let D=0;
+    if (Urav[7] == '-')
+        D = ((Urav[4] * Urav[4]) + (4 * Urav[0] * Urav[8]))
+    else
+        D = ((Urav[4] * Urav[4]) - (4 * Urav[0] * Urav[8]));
+    let squear = Math.sqrt(D);
+    if (Urav[3] == '-')
+        x1 = (+(Urav[4]) + squear) / (2 * Urav[0]);
+    else
+        x1 = (+(-Urav[4]) + squear) / (2 * Urav[0]);
+
+    if (Urav[3] == '-')
+        x2 = (+(Urav[4]) - squear) / (2 * Urav[0]);
+    else
+        x2 = (+(-Urav[4]) - squear) / (2 * Urav[0]);
+
+    result.push(x1,x2);
+    return result;
 }
+
